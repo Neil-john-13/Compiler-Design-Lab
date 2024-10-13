4fd255363d91caaf4cfb897ab8513df606824652
Neil-john-13 <117030166+Neil-john-13@users.noreply.github.com>
2024-10-14 00:05:50 +0530

    Update recursive descent parser.c which shows the parsing now

--- recursive descent parser/recursive descent parser.c
+++ recursive descent parser/recursive descent parser.c
@@ -3,13 +3,64 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
-char expr[25];
+char expr[25],pro[25];
 int ptr,len;
+void replace(char oldSub[], char newSub[])
+{
+    char result[1000];
+    int i, j = 0, k;
+    int oldLen = strlen(oldSub);
+    int newLen = strlen(newSub);
+    int found = 0;
+    
+    for (i = 0; pro[i] != '\0'; i++) {
 
+        if (strncmp(&pro[i], oldSub, oldLen) == 0) {
+
+            for (k = 0; k < newLen; k++) {
+                result[j++] = newSub[k];
+            }
+            i += oldLen - 1; 
+            found = 1;
+        } else {
+            result[j++] = pro[i];
+        }
+    }
+    result[j] = '\0';  
+    strcpy(pro,result);
+
+    if (found)
+        printf("%-25s\t", result);
+    else
+        printf("Substring not found.\n");
+}
+void _remove(char sub[]) 
+{
+    char result[1000];
+    int i, j = 0, k;
+    int subLen = strlen(sub);
+    int found = 0;
+
+    for (i = 0; pro[i] != '\0'; i++) {
+        
+        if (strncmp(&pro[i], sub, subLen) == 0) {
+            i += subLen - 1; 
+            found = 1;
+        } else {
+            result[j++] = pro[i];
+        }
+    }
+    result[j] = '\0';  
+    strcpy(pro,result);
+    if (found)
+        printf("%-25s\t", result);
+    else
+        printf("Substring not found.\n");
+}
 void E()
 {
-    printf("\nE->TE'");
+    printf("E->TE'\n");
+    replace("E","TE'");
     T();
     E_();
 }
@@ -18,19 +69,22 @@ void E_()
     
     if(expr[ptr]=='+' && ptr<len)
     {
-        printf("\nE'->+TE'");
+        printf("E'->+TE'\n");
+        replace("E'","+TE'");
         ptr++;
         T();
         E_();
     }
     else{
-        printf("\nE'->e");
+        printf("E'->e\n");
+        _remove("E'");
         return;
     }
 }
 void T()
 {
-    printf("\nT->FT'");
+    printf("T->FT'\n");
+    replace("T","FT'");
     F();
     T_();
 }
@@ -39,13 +93,15 @@ void T_()
     
     if(expr[ptr]=='*' && ptr<len)
     {
-        printf("\nT'->*FT'");
+        printf("T'->*FT'\n");
+        replace("T'","*FT'");
         ptr++;
         F();
         T_();
     }
     else{
-        printf("\nT'->e");
+        printf("T'->e\n");
+        _remove("T'");
     }
 }
 void F()
@@ -53,7 +109,11 @@ void F()
     
    if(expr[ptr]>='0' && expr[ptr]<='9' &&ptr<len)
     {
-        printf("\nF->i");
+        printf("F->i\n");
+        char str[3];
+        str[0]=expr[ptr];
+        str[1]='\0';
+        replace("F",str);
         ptr++;
     }
 }
@@ -65,10 +125,12 @@ void main()
     printf("\n Enter the input :");
     scanf("%s",expr);
     len=strlen(expr);
+    strcpy(pro,"E");
+    printf("E                            \t");
     E();
     if(ptr==len && expr[ptr]=='\0')
     {
-        printf("\nString accepted");
+        printf("String accepted");
     }
     else
     {
@@ -83,16 +145,15 @@ Grammar Parser
          T->FT' 
          T'->*FT'|e 
          F->i
- Enter the input :2+7
-
-E->TE'
-T->FT'
-F->i
-T'->e
-E'->+TE'
-T->FT'
-F->i
-T'->e
-E'->e
-String accepted
+ Enter the input :3+7
+E                               E->TE'
+TE'                             T->FT'
+FT'E'                           F->i
+3T'E'                           T'->e
+3E'                             E'->+TE'
+3+TE'                           T->FT'
+3+FT'E'                         F->i
+3+7T'E'                         T'->e
+3+7E'                           E'->e
+3+7                             String accepted
 */
